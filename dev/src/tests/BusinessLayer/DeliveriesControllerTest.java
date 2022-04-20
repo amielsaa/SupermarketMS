239743b03febbd04216e0fc758d2bd4b8e50fdea
Shoval Yarkoni <shoval.yarkoni@gmail.il>
2022-04-20 20:57:44 +0300

    created deliveriesControllerTest file

---
+++ dev/src/tests/BusinessLayer/DeliveriesControllerTest.java
@@ -0,0 +1,99 @@
+package BusinessLayer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeliveriesControllerTest {
+    DeliveriesController deliveriesController;
+
+    @BeforeEach
+    void setUp() {
+        this.deliveriesController=new DeliveriesController();
+    }
+
+    @Test
+    void addDelivery() {
+    }
+
+    @Test
+    void getCompletedDelivery() {
+    }
+
+    @Test
+    void getUpcomingDelivery() {
+    }
+
+    @Test
+    void getUpcomingDeliveries() {
+    }
+
+    @Test
+    void getDeliveryArchive() {
+    }
+
+    @Test
+    void addDestination() {
+    }
+
+    @Test
+    void removeDestination() {
+    }
+
+    @Test
+    void addItemToDestination() {
+    }
+
+    @Test
+    void removeItemFromDestination() {
+    }
+
+    @Test
+    void editItemQuantity() {
+    }
+
+    @Test
+    void getItemsOfDest() {
+    }
+
+    @Test
+    void editStartTime() {
+    }
+
+    @Test
+    void editEndTime() {
+    }
+
+    @Test
+    void editDriver() {
+    }
+
+    @Test
+    void editTruck() {
+    }
+
+    @Test
+    void setOrigin() {
+    }
+
+    @Test
+    void setWeight() {
+    }
+
+    @Test
+    void completeDelivery() {
+    }
+
+    @Test
+    void checkTruckHasUpcomingDelivery() {
+    }
+
+    @Test
+    void checkSiteHasUpcomingDelivery() {
+    }
+
+    @Test
+    void checkDriverHasUpcomingDelivery() {
+    }
+}
