49d6c02f4a018c4cbbc9e12448a6bb2db478454f
feliks1 <feliks@post.bgu.ac.il>
2022-05-06 23:09:58 +0300

    editing DAL

--- dev/src/DAL/OrderDAO.java
+++ dev/src/DAL/OrderDAO.java
@@ -2,6 +2,9 @@ package DAL;
 
 import BusinessLayer.Order;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class OrderDAO extends DalController {
@@ -14,7 +17,69 @@ public class OrderDAO extends DalController {
     }
 
     public void select(){}
-    public void insert(){}
-    public void delete(){}
     //public void update(){}
+
+    public void insertOrders(int bn, int orderID, int finalprice, String orderdate ) throws Exception {
+        String sql = "INSERT INTO Orders(bn, orderID, finalprice, orderdate ) VALUES(?,?,?,?)";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.setInt(2, orderID);
+            pstmt.setInt(3, finalprice);
+            pstmt.setString(4, orderdate);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void deleteOrders(int bn, int orderID) throws Exception {
+        String sql = "DELETE FROM Orders WHERE bn = ?, orderID = ?";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.setInt(2, orderID);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void insertOrderItems(int orderID, int itemname, String itemproducer, int itemprice, int itemamount ) throws Exception {
+        String sql = "INSERT INTO OrderItems(orderID, itemname, itemproducer, itemprice, itemamount) VALUES(?,?,?,?,?)";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, orderID);
+            pstmt.setInt(2, itemname);
+            pstmt.setString(3, itemproducer);
+            pstmt.setInt(4, itemprice);
+            pstmt.setInt(5, itemamount);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void deleteOrderItems(int orderID, int itemname, String itemproducer) throws Exception {
+        String sql = "DELETE FROM OrderItems WHERE orderID = ?, itemname = ?, itemproducer = ?";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, orderID);
+            pstmt.setInt(2, itemname);
+            pstmt.setString(3, itemproducer);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+
 }
--- dev/src/DAL/SupplierDAO.java
+++ dev/src/DAL/SupplierDAO.java
@@ -17,7 +17,6 @@ public class SupplierDAO extends DalController {
     }
 
     public void select(){}
-    public void delete(){}
 
     public void insertSupplier(int bn, String name, int bankaccount, int paymentdetails, int deliverybydays, int selfdelivery) throws Exception {
         String sql = "INSERT INTO Suppliers(bn, name, bankaccount, paymentdetails, deliverybydays, selfdelivery) VALUES(?,?,?,?,?,?)";
@@ -37,6 +36,19 @@ public class SupplierDAO extends DalController {
         }
     }
 
+    public void deleteSupplier(int bn) throws Exception {
+        String sql = "DELETE FROM Suppliers WHERE bn = ?";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
     public void insertQuantityAgreement(int bn, String itemname, String producer, int price) throws Exception {
         String sql = "INSERT INTO QuantityAgreement(bn, itemname, producer, price) VALUES(?,?,?,?)";
 
@@ -47,6 +59,21 @@ public class SupplierDAO extends DalController {
             pstmt.setString(2, itemname);
             pstmt.setString(3, producer);
             pstmt.setDouble(4, price);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void deleteQuantityAgreement(int bn, String itemname, String producer) throws Exception {
+        String sql = "DELETE FROM QuantityAgreement WHERE bn = ?, itemname = ?, producer = ?";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.setString(2, itemname);
+            pstmt.setString(3, producer);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new Exception(e.getMessage());
@@ -68,8 +95,36 @@ public class SupplierDAO extends DalController {
         }
     }
 
+    public void deleteContact(int bn, String name) throws Exception {
+        String sql = "DELETE FROM Contacts WHERE bn = ?, name = ?";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.setString(2, name);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
     public void insertDaysToDeliver(int bn, int day) throws Exception {
         String sql = "INSERT INTO DaysToDeliver(bn, day) VALUES(?,?)";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.setInt(2, day);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void deleteDaysToDeliver(int bn, int day) throws Exception {
+        String sql = "DELETE FROM DaysToDeliver WHERE bn = ?, day = ?";
 
         try{
             Connection conn = this.makeConnection();
@@ -93,6 +148,22 @@ public class SupplierDAO extends DalController {
             pstmt.setString(3, producer);
             pstmt.setInt(4, itemamount);
             pstmt.setInt(5, itemdiscount);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void deleteDiscounts(int bn, String itemname, String producer, int itemamount) throws Exception {
+        String sql = "DELETE FROM Discounts WHERE bn = ?, itemname = ?, producer = ?, itemamount = ?";
+
+        try{
+            Connection conn = this.makeConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bn);
+            pstmt.setString(2, itemname);
+            pstmt.setString(3, producer);
+            pstmt.setInt(4, itemamount);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new Exception(e.getMessage());
