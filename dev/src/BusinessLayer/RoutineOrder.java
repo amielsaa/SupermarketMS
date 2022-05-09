a2502d161df378ae68fff97123b3c104848aa415
feliks1 <feliks@post.bgu.ac.il>
2022-05-09 20:00:27 +0300

    minor fixes

---
+++ dev/src/BusinessLayer/RoutineOrder.java
@@ -0,0 +1,55 @@
+package BusinessLayer;
+
+import misc.Days;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RoutineOrder extends Order{
+    private Set<Days> Days_To_Deliver;
+
+
+    public RoutineOrder(int supplier_bn, int order_id, HashMap<Integer, OrderItem> item_num_to_orderItem, double final_price, Date order_date, Set<Integer> days_To_Deliver) {
+        super(supplier_bn, order_id, item_num_to_orderItem, final_price, order_date);
+        Days_To_Deliver = setDays_To_Deliver(days_To_Deliver);
+    }
+
+    public RoutineOrder(int supplier_bn, int order_id, double final_price, String order_date, Set<Integer> days_To_Deliver) {
+        super(supplier_bn, order_id, final_price, order_date);
+        Days_To_Deliver = setDays_To_Deliver(days_To_Deliver);
+    }
+
+    public Set<Days> setDays_To_Deliver(Set<Integer> days_To_Deliver) {
+        Integer[] days = new Integer[days_To_Deliver.toArray().length];//gets the set to Array
+        Set<Days> daysSet=new LinkedHashSet<Days>();
+        for(int i=0; i<days.length;i++){
+            days[i] = (Integer)days_To_Deliver.toArray()[i];
+        }
+        for (int i:days){
+            daysSet.add(dayConvertor(i));
+        }
+        return daysSet;
+    }
+
+    private Days dayConvertor(int day){
+        if(day==1)
+            return Days.sunday;
+        else if(day==2)
+            return Days.monday;
+        else if(day==3)
+            return Days.tuesday;
+        else if(day==4)
+            return Days.wednesday;
+        else if(day==5)
+            return Days.thursday;
+        else if(day==6)
+            return Days.friday;
+        else if(day==7)
+            return Days.saturday;
+        else
+            throw new IllegalArgumentException("day is not valid");
+
+    }
+}
--- dev/src/BusinessLayer/Supplier.java
+++ dev/src/BusinessLayer/Supplier.java
@@ -17,11 +17,11 @@ public class Supplier  {
     private QuantityAgreement Quantity_Agreement;
     private boolean Delivery_By_Days; //if the supplier Delivers by days or not
     private boolean Self_Delivery_Or_Pickup;// if we need to pick-up or he delivers us
-    private Set<Days> Days_To_Deliver;
 
 
 
-    public Supplier(String name, int business_num, int bank_acc_num, String payment_details, String contactName, String contactPhone, HashMap item_num_to_price, HashMap item_num_to_discount, HashMap item_num_to_name, boolean self_delivery_or_pickup, Set<Integer> days_to_deliver) {
+
+    public Supplier(String name, int business_num, int bank_acc_num, String payment_details, String contactName, String contactPhone, HashMap item_num_to_price, HashMap item_num_to_discount, HashMap item_num_to_name, boolean self_delivery_or_pickup) {
         Name = name;
         CheckLegalNumber(business_num);
         CheckLengthOfBusinessNumber(business_num);
@@ -34,10 +34,7 @@ public class Supplier  {
         Delivery_By_Days = false;
         Self_Delivery_Or_Pickup = self_delivery_or_pickup;
         Quantity_Agreement = new QuantityAgreement(item_num_to_price, item_num_to_discount, item_num_to_name);
-        if(!Delivery_By_Days)
-            Days_To_Deliver = new LinkedHashSet<Days>();
-        else
-            Days_To_Deliver=setDays_To_Deliver(days_to_deliver);
+
     }
 
     public Supplier(int business_num, String name, int bank_acc_num, String payment_details, int delivery_By_Days, int self_delivery_or_pickup) {
@@ -50,7 +47,6 @@ public class Supplier  {
         Payment_Details =setPayment_Details(payment_details);
         Contacts = new LinkedList<Contact>();
         Quantity_Agreement = new QuantityAgreement();
-        Days_To_Deliver = new LinkedHashSet<Days>();
     }
 
     public void setName(String name) {
@@ -89,10 +85,6 @@ public class Supplier  {
         return Quantity_Agreement;
     }
 
-    public Set<Days> getDays_To_Deliver() {
-        return Days_To_Deliver;
-    }
-
     public void setBank_Acc_Num(int bank_Acc_Num) {
         Bank_Acc_Num = bank_Acc_Num;
     }
@@ -105,19 +97,7 @@ public class Supplier  {
         Contacts = contacts;
     }
 
-    public Set<Days> setDays_To_Deliver(Set<Integer> days_To_Deliver) {
-        Integer[] days = new Integer[days_To_Deliver.toArray().length];//gets the set to Array
-        Set<Days> daysSet=new LinkedHashSet<Days>();
-        for(int i=0; i<days.length;i++){
-            days[i] = (Integer)days_To_Deliver.toArray()[i];
-        }
-        for (int i:days){
-            daysSet.add(dayCovertor(i));
-        }
 
-
-        return daysSet;
-    }
 
     public void setDelivery_By_Days(boolean delivery_By_Days) {
         Delivery_By_Days = delivery_By_Days;
@@ -180,42 +160,24 @@ public class Supplier  {
         if(numberToString.length()!=9)
             throw new IllegalArgumentException("Business number should be 9 digits");
     }
-    public void addSupplierDeliveryDay(int day){
-        if(!Delivery_By_Days)
-            Delivery_By_Days=true;
-        Days dayToAdd=dayCovertor(day);
-        if (Days_To_Deliver.contains(dayToAdd))
-            throw new IllegalArgumentException("this Day is already in the Delivery days");
-        Days_To_Deliver.add(dayToAdd);
+//    public void addSupplierDeliveryDay(int day){
+//        if(!Delivery_By_Days)
+//            Delivery_By_Days=true;
+//        Days dayToAdd=dayCovertor(day);
+//        if (Days_To_Deliver.contains(dayToAdd))
+//            throw new IllegalArgumentException("this Day is already in the Delivery days");
+//        Days_To_Deliver.add(dayToAdd);
+//
+//
+//    }
+//    public void removeSupplierDeliveryDay(int day){
+//        Days dayToRemove=dayCovertor(day);
+//        if(!Days_To_Deliver.contains(dayToRemove))
+//            throw new IllegalArgumentException("cannot delete day-day isn't in the delivery days");
+//        Days_To_Deliver.remove(dayToRemove);
+//
+//    }
 
-
-    }
-    public void removeSupplierDeliveryDay(int day){
-        Days dayToRemove=dayCovertor(day);
-        if(!Days_To_Deliver.contains(dayToRemove))
-            throw new IllegalArgumentException("cannot delete day-day isn't in the delivery days");
-        Days_To_Deliver.remove(dayToRemove);
-
-    }
-    private Days dayCovertor(int day){
-        if(day==1)
-            return Days.sunday;
-        else if(day==2)
-            return Days.monday;
-        else if(day==3)
-            return Days.tuesday;
-        else if(day==4)
-            return Days.wednesday;
-        else if(day==5)
-            return Days.thursday;
-        else if(day==6)
-            return Days.friday;
-        else if(day==7)
-            return Days.saturday;
-        else
-            throw new IllegalArgumentException("day is not valid");
-
-    }
     public void  updateSupplierBankAccount(int bank_acc){
         CheckLegalNumber(bank_acc);
         setBank_Acc_Num(bank_acc);
