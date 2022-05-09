3a948e1c25ad6c38069b612d476c0f2533bb2a6e
Your Name <you@example.com>
2022-05-09 22:38:44 +0300

    fixed some service shit

--- dev/src/ServiceLayer/DummyObjects/DQuantityAgreement.java
+++ dev/src/ServiceLayer/DummyObjects/DQuantityAgreement.java
@@ -1,55 +1,56 @@
 package ServiceLayer.DummyObjects;
 
 import BusinessLayer.QuantityAgreement;
+import misc.Pair;
 
 import java.util.HashMap;
 
 public class DQuantityAgreement {
 
-    private HashMap<Integer, Double> item_Num_To_Price;
-    private HashMap<Integer,HashMap<Integer,Integer>> item_Num_To_Discount;
-    private HashMap<Integer,String> item_Num_To_Name;
+    private HashMap<Pair<String,String>, Double> item_Num_To_Price;
+    private HashMap<Pair<String,String>,HashMap<Integer,Integer>> item_Num_To_Discount;
+
 
 
     public DQuantityAgreement(QuantityAgreement agreement) {
-        item_Num_To_Price = new HashMap<Integer, Double>();
-        for(int x : agreement.getItem_To_Price().keySet()) {
+        item_Num_To_Price = new HashMap<Pair<String,String>, Double>();
+        for(Pair x : agreement.getItem_To_Price().keySet()) {
             item_Num_To_Price.put(x, agreement.getItem_To_Price().get(x).doubleValue());
         }
 
 
-        item_Num_To_Discount = new HashMap<Integer,HashMap<Integer,Integer>>();
-        for(int x : agreement.getItem_Num_To_Discount().keySet()) {
+        item_Num_To_Discount = new HashMap<Pair<String,String>,HashMap<Integer,Integer>>();
+        for(Pair x : agreement.getItem_Num_To_Discount().keySet()) {
             item_Num_To_Discount.put(x, new HashMap<Integer,Integer>());
         }
-        for(int x : agreement.getItem_Num_To_Discount().keySet()) {
+        for(Pair x : agreement.getItem_Num_To_Discount().keySet()) {
             for(int y : agreement.getItem_Num_To_Discount().get(x).keySet()) {
                 item_Num_To_Discount.get(x).put(y, agreement.getItem_Num_To_Discount().get(x).get(y));
             }
         }
-
+        /*
         item_Num_To_Name = new HashMap<Integer,String>();
         for(int x : agreement.getItem_Num_To_Name().keySet()) {
             item_Num_To_Name.put(x, agreement.getItem_Num_To_Name().get(x));
         }
+
+         */
     }
 
-    public HashMap<Integer, Double> getItem_Num_To_Price() {
+    public HashMap<Pair<String,String>, Double> getItem_Num_To_Price() {
         return item_Num_To_Price;
     }
 
-    public HashMap<Integer, HashMap<Integer, Integer>> getItem_Num_To_Discount() {
+    public HashMap<Pair<String,String>, HashMap<Integer, Integer>> getItem_Num_To_Discount() {
         return item_Num_To_Discount;
     }
 
-    public HashMap<Integer, String> getItem_Num_To_Name() {
-        return item_Num_To_Name;
-    }
+
 
     public String toString(){
         String ret = "\n";
-        for(int x:getItem_Num_To_Name().keySet()){
-            ret += ("Item ID: " + x + ", Item name: " + getItem_Num_To_Name().get(x) + ", Item price: " + getItem_Num_To_Price().get(x)) + "\n";
+        for(Pair x:getItem_Num_To_Price().keySet()){
+            ret += ("Item Name: " + x.getFirst() + ", Item Producer: " + x.getSecond() + ", Item price: " + getItem_Num_To_Price().get(x)) + "\n";
         }
         return ret;
         //todo: add discount print?
--- dev/src/ServiceLayer/DummyObjects/DSupplier.java
+++ dev/src/ServiceLayer/DummyObjects/DSupplier.java
@@ -16,17 +16,17 @@ public class DSupplier {
     private int Business_Num;
     private List<DContact> Contacts;
     private DQuantityAgreement Quantity_Agreement;
-    private boolean Delivery_By_Days; //if the supplier Delivers by days or not
+
     private boolean Self_Delivery_Or_Pickup;// if we need to pick-up or he delivers us
-    private Set<Days> Days_To_Deliver;
+
 
 
     public DSupplier(Supplier s) {
         Name = s.getName();
         Business_Num = s.getBusiness_Num();
-        Delivery_By_Days = s.isDelivery_By_Days();
+
         Self_Delivery_Or_Pickup = s.isSelf_Delivery_Or_Pickup();
-        Days_To_Deliver = s.getDays_To_Deliver();
+
         Contacts = new LinkedList<DContact>();
         for(Contact c : s.getContacts())
             Contacts.add(new DContact(c));
@@ -49,19 +49,15 @@ public class DSupplier {
         return Quantity_Agreement;
     }
 
-    public boolean isDelivery_By_Days() {
-        return Delivery_By_Days;
-    }
+
 
     public boolean isSelf_Delivery_Or_Pickup() {
         return Self_Delivery_Or_Pickup;
     }
 
-    public Set<Days> getDays_To_Deliver() {
-        return Days_To_Deliver;
-    }
+
 
     public String toString(){
-        return "Supplier name: " + getName() + " ,Business number: " + getBusiness_Num() +" ,Is self delivering: " + isSelf_Delivery_Or_Pickup() + "\nIs delivering by days: " +isDelivery_By_Days() + " ,Days to deliver: " + getDays_To_Deliver() + "\nContacts: " + getContacts().toString() + "\nQuantity Agreement: " + getQuantity_Agreement();
+        return "Supplier name: " + getName() + " ,Business number: " + getBusiness_Num() +" ,Is self delivering: " + isSelf_Delivery_Or_Pickup() + "\nContacts: " + getContacts().toString() + "\nQuantity Agreement: " + getQuantity_Agreement();
     }
 }
