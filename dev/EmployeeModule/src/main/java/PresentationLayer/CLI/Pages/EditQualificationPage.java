1bd340d969e97295f9bba415b818f2e5aecad7ce
talrozi <talrozi@post.bgu.ac.il>
2022-05-10 19:33:04 +0300

    Finished the NICLI Implementation.

---
+++ dev/EmployeeModule/src/main/java/PresentationLayer/CLI/Pages/EditQualificationPage.java
@@ -0,0 +1,92 @@
+package PresentationLayer.CLI.Pages;
+
+import BusinessLayer.Employee;
+import BusinessLayer.Permission;
+import BusinessLayer.Qualification;
+import ServiceLayer.Gateway;
+import Utilities.CLIException;
+import Utilities.Pair;
+import Utilities.PrettyTable;
+import Utilities.Response;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import static Utilities.PrettyInput.printAndWaitForLegalInt;
+import static Utilities.PrettyInput.printAndWaitForLegalString;
+import static Utilities.PrettyPrint.*;
+import static Utilities.Util.checkLegalId;
+
+public class EditQualificationPage extends OptionsMenuPage
+{
+    private ResponsePage<Boolean> pgEmptyFalse = new ReturnPage();
+
+    private Qualification qualification;
+
+    @Override
+    public Boolean runWithResponse(Scanner input, Gateway g) throws CLIException {
+        String qualificationName = printAndWaitForLegalString(input, "Enter the name of the qualification you wish to edit: ");
+        Response<Qualification> res_qual = g.getQualification(qualificationName);
+        if(res_qual.isSuccess()){
+            this.qualification = res_qual.getData();
+            super.runWithResponse(input, g);
+        }
+        else {
+            System.out.println(makeErrorMessage("Failed to show qualification, " + res_qual.getMessage()));
+        }
+        return true;
+    }
+
+    @Override
+    public Map<String, ResponsePage<Boolean>> getOptionsMap()
+    {
+        return new LinkedHashMap<String, ResponsePage<Boolean>>() {{
+            put("Return", pgEmptyFalse);
+            put("Add Permission to Qualification", makeResponsePage((Pair<Scanner, Gateway> args) -> {
+                try {
+                    String perm_name_add = printAndWaitForLegalString(args.getKey(), "Name of the permission to add: ");
+                    Response<Qualification> res_perm_add = args.getValue().addPermissionToQualification(perm_name_add, qualification.getName());
+                    if(!res_perm_add.isSuccess()){
+                        throw new CLIException(res_perm_add.getMessage());
+                    }
+                    System.out.println(makeSuccessMessage());
+                } catch (Exception e) {
+                    System.out.println(makeErrorMessage(e.getMessage()));
+                } finally {
+                    return true;
+                }
+            }));
+            put("Remove Permission from Qualification", makeResponsePage((Pair<Scanner, Gateway> args) -> {
+                try {
+                    List<Permission> permissions = qualification.getPermissions();
+                    System.out.println(makeBigTitle("Permissions of " + qualification.getName()));
+                    PrettyTable table = new PrettyTable("Name");
+                    for (Permission p : permissions)
+                    {
+                        table.insert(p.getName());
+                    }
+                    System.out.println(table.toString());
+
+                    String perm_name_rem2 = printAndWaitForLegalString(args.getKey(), "Name of the permission to remove: ");
+                    Response<Qualification> res_perm_rem2 = args.getValue().removePermissionFromQualification(perm_name_rem2, qualification.getName());
+                    if(!res_perm_rem2.isSuccess()){
+                        throw new CLIException(res_perm_rem2.getMessage());
+                    }
+                    System.out.println(makeSuccessMessage());
+                } catch (Exception e) {
+                    System.out.println(makeErrorMessage(e.getMessage()));
+                } finally {
+                    return true;
+                }
+            }));
+        }};
+    }
+
+    @Override
+    public String getTitle()
+    {
+        return "Editing " + qualification.getName();
+    }
+}
