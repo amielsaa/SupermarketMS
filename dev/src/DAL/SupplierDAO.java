9f49b4cebc00195b31bf25ce4fdd665b7e69e296
feliks1 <feliks@post.bgu.ac.il>
2022-05-11 19:54:38 +0300

    presentation fixes + DAL fixes + merge with Ari (fixing db connection ATM)

--- dev/src/DAL/SupplierDAO.java
+++ dev/src/DAL/SupplierDAO.java
@@ -1,7 +1,6 @@
 package DAL;
 
 import BusinessLayer.Supplier;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
