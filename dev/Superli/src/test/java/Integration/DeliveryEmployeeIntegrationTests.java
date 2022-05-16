49591434322fe28edac4eb2ecad7ab75811754cb
Yuval Segev <syuva@post.bgu.ac.il>
2022-05-16 15:49:29 +0300

    integration tests

--- dev/Superli/src/test/java/Integration/DeliveryEmployeeIntegrationTests.java
+++ dev/Superli/src/test/java/Integration/DeliveryEmployeeIntegrationTests.java
@@ -1,11 +1,17 @@
 package Integration;
 
+import DeliveryModule.BusinessLayer.DeliveryZone;
 import DeliveryModule.BusinessLayer.Driver;
 import DeliveryModule.ServiceLayer.DeliveryService;
-import EmployeeModule.BusinessLayer.Employee;
+import EmployeeModule.BusinessLayer.*;
 import EmployeeModule.ServiceLayer.Gateway;
+import Utilities.Response;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -13,6 +19,7 @@ public class DeliveryEmployeeIntegrationTests {
     private Gateway EmployeeMod;
     private DeliveryService DeliveryMod;
     private final int ADMIN_UID = 580083434;
+    private BankAccountDetails defaultBankAccountDetails;
     @Before
     public void setUp()
     {
@@ -30,72 +37,176 @@ public class DeliveryEmployeeIntegrationTests {
         DeliveryMod = EmployeeMod.getDeliveryService().getData();
         EmployeeMod.clearDatabases();
         DeliveryMod.clearDatabases();
+        defaultBankAccountDetails = new BankAccountDetails(0, 1, 0, "Bank", "Branch", "Bob");
+
+    }
+
+    @Test
+    public void testAddDriver()
+    {
+        EmployeeMod.login(ADMIN_UID);
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        assertTrue(DeliveryMod.getDriver(100100100).isSuccess());
+    }
+
+    @Test
+    public void testDeleteDriverQual()
+    {
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.driverRemoveQualification(100100100);
+        assertFalse(DeliveryMod.getDriver(100100100).isSuccess());
     }
 
     @Test
     public void testDeleteDriver()
     {
-        DeliveryMod.addDriver(100100100, "driver 1", "C");
-        DeliveryMod.addDriver(100100101, "driver 2", "C");
-        DeliveryMod.deleteDriver(100100101);
-        assertTrue(DeliveryMod.getDriver(100100100).isSuccess());
-        assertFalse(DeliveryMod.getDriver(100100101).isSuccess());
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.removeEmployee(100100100);
+        assertFalse(DeliveryMod.getDriver(100100100).isSuccess());
     }
 
     @Test
-    public void testEditDriver()
+    public void testEditDriverName()
     {
-        DeliveryMod.addDriver(100100100, "driver 1", "C");
-        DeliveryMod.editDriverName(100100100, "driver 2");
-        DeliveryMod.editDriverLicenseType(100100100, "C1");
-        assertEquals("Id: 100100100\n\t* Name: driver 2\n\t* License type: C1\n", DeliveryMod.getDriver(100100100).getData());
+
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.updateEmployeeName(100100100, "driver 2");
+        assertTrue(DeliveryMod.getDriver(100100100).getData().toString().contains("driver 2"));
     }
 
     @Test
-    public void test3()
+    public void testDeliveryApplication()
     {
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
 
+        Map<Integer, List<String>> hashmap  = new HashMap<Integer, List<String>>() {{
+            put(100100100, Arrays.asList("Driver"));
+            put(2, Arrays.asList("WarehouseWorker"));
+        }};
+
+        addShift(hashmap, 1000, LocalDateTime.of(2024, Month.MAY, 20, 1, 1), ShiftTime.DAY);
+        DeliveryMod.addTruck(1234567, "a", 10000);
+        DeliveryMod.addSupplierWarehouse("a", 0, "a","a");
+        DeliveryMod.addBranch("b", 0, "b","B");
+        assertTrue(DeliveryMod.addDelivery(LocalDateTime.of(2024, Month.MAY, 20, 1, 2), LocalDateTime.of(2024, Month.MAY, 20, 1, 3), 1234567, 100100100, 1, 2).isSuccess());
     }
 
     @Test
-    public void test4()
+    public void testDeliveryApplicationFail()
     {
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
 
+        Map<Integer, List<String>> hashmap  = new HashMap<Integer, List<String>>() {{
+            put(100100100, Arrays.asList("Driver"));
+            put(2, Arrays.asList("WarehouseWorker"));
+        }};
+
+        addShift(hashmap, 1000, LocalDateTime.of(2024, Month.MAY, 20, 1, 1), ShiftTime.DAY);
+        DeliveryMod.addTruck(1234567, "a", 10000);
+        DeliveryMod.addSupplierWarehouse("a", 0, "a","a");
+        DeliveryMod.addBranch("b", 0, "b","B");
+        assertFalse(DeliveryMod.addDelivery(LocalDateTime.of(2024, Month.APRIL, 20, 1, 2), LocalDateTime.of(2024, Month.APRIL, 20, 1, 3), 1234567, 100100100, 1, 2).isSuccess());
     }
 
     @Test
-    public void test5()
+    public void driverRemovalFailOnBeingAppliedToDeliveryTest()
     {
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
 
+        Map<Integer, List<String>> hashmap  = new HashMap<Integer, List<String>>() {{
+            EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+            EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
+            put(100100100, Arrays.asList("Driver"));
+            put(2, Arrays.asList("WarehouseWorker"));
+        }};
+
+        addShift(hashmap, 1000, LocalDateTime.of(2024, Month.MAY, 20, 1, 1), ShiftTime.DAY);
+        DeliveryMod.addTruck(1234567, "a", 10000);
+        DeliveryMod.addSupplierWarehouse("a", 0, "a","a");
+        DeliveryMod.addBranch("b", 0, "b","B");
+        DeliveryMod.addDelivery(LocalDateTime.of(2024, Month.MAY, 20, 1, 2), LocalDateTime.of(2024, Month.MAY, 20, 1, 3), 1234567, 100100100, 1, 2).isSuccess();
+        assertFalse(EmployeeMod.removeEmployee(100100100).isSuccess());
     }
 
     @Test
-    public void test6()
+    public void failToMakeDeliveryOnNoWarehouseWorkerTest()
     {
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
 
+        Map<Integer, List<String>> hashmap  = new HashMap<Integer, List<String>>() {{
+            EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+            EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
+            put(100100100, Arrays.asList("Driver"));
+        }};
+
+        addShift(hashmap, 1000, LocalDateTime.of(2024, Month.MAY, 20, 1, 1), ShiftTime.DAY);
+        DeliveryMod.addTruck(1234567, "a", 10000);
+        DeliveryMod.addSupplierWarehouse("a", 0, "a","a");
+        DeliveryMod.addBranch("b", 0, "b","B");
+        assertFalse(DeliveryMod.addDelivery(LocalDateTime.of(2024, Month.MAY, 20, 1, 2), LocalDateTime.of(2024, Month.MAY, 20, 1, 3), 1234567, 100100100, 1, 2).isSuccess());
     }
 
     @Test
-    public void test7()
+    public void failToMakeDeliveryOnNoDriverInShiftTest()
     {
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
 
+        Map<Integer, List<String>> hashmap  = new HashMap<Integer, List<String>>() {{
+            EmployeeMod.addEmployee(2, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+            EmployeeMod.employeeAddQualification(2, EmployeeMod.addQualification("WarehouseWorker").getData());
+            put(2, Arrays.asList("WarehouseWorker"));
+        }};
+
+        addShift(hashmap, 1000, LocalDateTime.of(2024, Month.MAY, 20, 1, 1), ShiftTime.DAY);
+        DeliveryMod.addTruck(1234567, "a", 10000);
+        DeliveryMod.addSupplierWarehouse("a", 0, "a","a");
+        DeliveryMod.addBranch("b", 0, "b","B");
+        assertFalse(DeliveryMod.addDelivery(LocalDateTime.of(2024, Month.MAY, 20, 1, 2), LocalDateTime.of(2024, Month.MAY, 20, 1, 3), 1234567, 100100100, 1, 2).isSuccess());
     }
 
     @Test
-    public void test8()
+    public void changeLicenseTypeTest()
     {
-
+        EmployeeMod.addEmployee(100100100, "driver 1", defaultBankAccountDetails, 1000, LocalDateTime.now(), "");
+        EmployeeMod.driverAddQualification(100100100, "C");
+        DeliveryMod.editDriverLicenseType(1, "C1");
+        assertTrue(DeliveryMod.getDriver(100100100).getData().toString().contains("C1"));
     }
 
-    @Test
-    public void test9()
+    private void addShift(Map<Integer, List<String>> workers, int ID_MANAGER, LocalDateTime time, ShiftTime shiftTime)
     {
-
+        Employee manager = EmployeeMod.addEmployee(ID_MANAGER, "Manager", bankAccountDetailsGenerator(ID_MANAGER), 42000, LocalDateTime.now(), "Branch 1 manager").getData();
+        EmployeeMod.logout();
+        EmployeeMod.login(ID_MANAGER);
+        EmployeeMod.addShift(1, time, manager, workers, shiftTime);
+        EmployeeMod.logout();
+        EmployeeMod.login(ADMIN_UID);
     }
 
-    @Test
-    public void test10()
-    {
-
+    private BankAccountDetails bankAccountDetailsGenerator(int seed) {
+        Random generator = new Random(seed);
+        int bankId = generator.nextInt();
+        int branchId = generator.nextInt();
+        int accountId = generator.nextInt();
+        String bankName = "BANK_NAMED_" + seed;
+        String branchName = "BRANCH_NAMED_" + seed;
+        String accountOwner = "ACCOUNT_NAMED_" + seed;
+        return new BankAccountDetails(bankId, branchId, accountId, bankName, branchName, accountOwner);
     }
 }
